d9862c01db90a1f6da5e704e0662357ef5328a0f
zhanbalazhan <sondana810gmail.com>
2022-01-07 12:30:36 -0500

    test2 message

--- LabGit/src/labGit2.java
+++ LabGit/src/labGit2.java
@@ -3,6 +3,7 @@ public class labGit2 {
 
 	public static void main(String[] args) {
 		// lab test
+		//test2
 
 	}
 
--- LabGit/src/labTets3.java
+++ LabGit/src/labTets3.java
@@ -3,6 +3,7 @@ public class labTets3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//test2
 
 	}
 
